5c59ac1d94bbca540452a0c321b1c7c47574b471
sboikov <sboikov@gridgain.com>
2017-03-01 18:36:27 +0300

    ignite-4705

--- modules/core/src/test/java/org/apache/ignite/testframework/junits/GridAbstractTest.java
+++ modules/core/src/test/java/org/apache/ignite/testframework/junits/GridAbstractTest.java
@@ -1369,6 +1369,7 @@ public abstract class GridAbstractTest extends TestCase {
 
         commSpi.setLocalPort(GridTestUtils.getNextCommPort(getClass()));
         commSpi.setTcpNoDelay(true);
+        commSpi.setIdleConnectionTimeout(500);
 
         cfg.setCommunicationSpi(commSpi);
 
